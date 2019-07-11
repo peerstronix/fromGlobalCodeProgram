d14b6acc605f8cff5cfba54a88f59ad4e2efa2c1
Nyarko Ebenezer <peerstronix@gmail.com>
2019-07-11 16:02:49 +0000

    eben

--- weather/weather.py
+++ weather/weather.py
@@ -1,10 +1,10 @@
 import pyowm
 
 owm = pyowm.OWM('1ba146ea396a1e63f568053174f294e0')
-observation = owm.weather_at_place('svalbard')
+observation = owm.weather_at_place('Accra, gh')
 w = observation.get_weather()
 
-print(w.get_wind())
+print(w.get_cordinate())
 print(w.get_humidity())
 print(w.get_temperature(unit='celsius'))
 
