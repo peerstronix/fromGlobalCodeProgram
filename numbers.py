26873ddbc4b7566e8e4f624fbb676f7336c76545
Nyarko Ebenezer <peerstronix@gmail.com>
2019-07-11 11:21:25 +0000

    eben

--- numbers.py
+++ numbers.py
@@ -1,6 +1,12 @@
-n = 5
 
-for i in range(0, n):
-	for j in range(0, i+1):
-		print("* ", end=" ")
-	print("\r")
+def printStars(n):
+	for i in range(0, n):
+		for j in range(0, i+1):
+			print("* ", end=" ")
+		print("\r")
+
+
+
+n = int(input("What  will be the maximum number of stars on a line?"))
+
+printStars(n)
